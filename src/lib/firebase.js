a4fd6843c2cb81e2a9921acebf8a54616564ce5f
Igor <ignatenkopo4ta@gmail.com>
2021-05-07 04:29:26 +0300

    fix firebase, fix styles

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,4 +1,6 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 import { firebaseConfig } from "../firebase/firebaseConfig";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
@@ -6,4 +8,4 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 export const dataBase = firebaseApp.firestore();
 export const auth = firebase.auth();
 
-export default firebaseApp;
+export { firebaseApp };
